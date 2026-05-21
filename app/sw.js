5b8e6e53c06bc4bae57e9f079e7dfe0be9eca2f1
Richard Odden <richard.odden@gmail.com>
2026-05-21 22:35:29 +0200

    Update CACHE_NAME to version v4

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'infobaaten-dev-v3';
+const CACHE_NAME = 'infobaaten-dev-v4';
 const urlsToCache = [
   '/infobaaten_development/app/',
   '/infobaaten_development/app/index.html',
