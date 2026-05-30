828d291c01cc84ed17ddbf6212e1b44e15cf5bc2
Richard Odden <richard.odden@gmail.com>
2026-05-30 21:49:18 +0200

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'infobaaten-dev-v8';
+const CACHE_NAME = 'infobaaten-dev-v2';
 const urlsToCache = [
   '/infobaaten_development/app/',
   '/infobaaten_development/app/index.html',
